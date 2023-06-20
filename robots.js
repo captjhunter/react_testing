063b428308bc184402561aca55d3463b62580fc0
captjhunter <hhjessica99@yahoo.com>
2023-06-20 10:31:22 -0400

    list of information to populate for robots

---
+++ robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Commander Shepard',
+        username: 'Shepard',
+        email: 'Shep.Normandy@alliance.net'
+    },
+    {
+        id: 2,
+        name: 'Liara T\'Soni',
+        username: 'Little Wing',
+        email: 'TsoniL@Thessia.net'
+    },
+    {
+        id: 3,
+        name: 'Tali vas Normandy',
+        username: 'TaliRannoch',
+        email: 'talibobali@migrantfleet.net'
+    },
+    {
+        id: 4,
+        name: 'Garrus Vakarian',
+        username: 'TurianRebel',
+        email: 'vakarianshamarian@palaven.net'
+    },
+    {
+        id: 5,
+        name: 'Wrex Urdnot',
+        username: 'WrexMerc',
+        email: 'wrexurdnot@tuchanka.net'
+    },
+    {
+        id: 6,
+        name: 'Ashley Williams',
+        username: 'Williams5',
+        email: 'ashsurvivor@alliance.net'
+    },
+    {
+        id: 7,
+        name: 'Kaidan Alenko',
+        username: 'bioticAlenko',
+        email: 'nonvirmiresurvivor@alliance.net'
+    },
+    {
+        id: 8,
+        name: 'Jeff Moreau',
+        username: 'Joker',
+        email: 'jokernormandy@alliance.net'
+    }
+]
